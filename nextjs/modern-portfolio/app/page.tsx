1191b1691548fcf78e98a407266fcd7329f5af41
Anirudh Sharma <anirudh03sharma@gmail.com>
2023-11-28 21:43:22 +0530

    Updated about and projects components in nextjs/modern-portfolio

--- nextjs/modern-portfolio/app/page.tsx
+++ nextjs/modern-portfolio/app/page.tsx
@@ -1,4 +1,6 @@
+import About from "@/components/about";
 import Intro from "@/components/intro";
+import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 
 const Home = () => { 
@@ -6,6 +8,8 @@ const Home = () => {
         <main className="flex flex-col items-center px-4">
             <Intro />
             <SectionDivider />
+            <About />
+            <Projects />
         </main>
     )
 };
