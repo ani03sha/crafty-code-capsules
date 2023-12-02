2206415378fed9f39e4e227858006f07b6f162eb
Anirudh Sharma <anirudh03sharma@gmail.com>
2023-12-02 10:02:54 +0530

    Updated header logic in nextjs/modern-portfolio

--- nextjs/modern-portfolio/app/layout.tsx
+++ nextjs/modern-portfolio/app/layout.tsx
@@ -1,13 +1,14 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Header from '@/components/header'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header"
+import ActiveSectionContextProvider from "@/context/active-section-context";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-    title: 'Anirudh | Portfolio',
-    description: 'Modern portfolio app',
+    title: "Stuart | The Minion",
+    description: "Modern portfolio app",
 }
 
 export default function RootLayout({
@@ -16,13 +17,18 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
+        <html
+            lang="en"
+            className="!scroll-smooth"
+        >
             <body className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}>
                 <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
                 <div className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem]
                 lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
-                <Header />
-                {children}
+                <ActiveSectionContextProvider>
+                    <Header />
+                    {children}
+                </ActiveSectionContextProvider>
             </body>
         </html>
     )
